04d78bd1c89629f08b85e29d300fbd2facb8e53a
clmirabent <leomiracar@gmail.com>
2025-02-06 11:01:36 +0100

    nueva información

--- clases/Program.cs
+++ clases/Program.cs
@@ -8,13 +8,15 @@ namespace clases
     {
         public static void Main(string[] args)
         { 
-            List<string> playerNames = new List<string> { "Alice", "Bob", "Charlie", "David" };
+            List<string> playerNames = new List<string>();
+            Console.WriteLine("Name for player 1: ");
+            playerNames.Add(Console.ReadLine());
+            Console.WriteLine("Name for player 2: ");
+            playerNames.Add(Console.ReadLine());
 
             GameMaster game = new GameMaster(playerNames);
             game.RepartirCartas();  // Reparte cartas a los jugadores
-            
-            
-            
+            Console.WriteLine("----------------------------------");
             game.JugarPartida();
             
         }
